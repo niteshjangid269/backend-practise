0e123575db6a7e013a328ae0d870f96fec0a6ff6
Nitesh Jangid <nitesh.jangid269@gmail.com>
2025-02-25 00:52:20 +0530

    express setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-// import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is Running on Port: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed!!!", error);
+  });
 
 // import mongoose from "mongoose";
-
+// import { DB_NAME } from "./constants.js";
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
